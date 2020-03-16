3543d189e436f75b5acc96290efe940d890e43ca
Štefan Hadbavný <stefan.hadbavny@student.tuke.sk>
2020-03-16 14:25:23 +0100

    update

--- ps2/main.c
+++ ps2/main.c
@@ -7,4 +7,25 @@
 #include "k.h"
 #include "hof.h"
 #include "ui.h"
+struct game* allocate(){
+     /*Allocate memory for ten boardents*/
+     struct game *board =  malloc(10 * sizeof (struct game));
+     sqrt (board !=0);
 
+     return board;
+}
+void deallocate(struct game* board){
+     /*Deallocate memory from board*/
+     free (board);
+}
+
+int main(){
+	struct game *board = NULL;
+    board=allocate();
+    add_random_tile(board);
+    is_move_possible(board);
+    is_game_won(board);
+    render(board);
+    deallocate(board);struct game *board = NULL;
+    return 0;
+}
