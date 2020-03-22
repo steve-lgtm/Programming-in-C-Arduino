6410e4183e2fcac6d25913394f1ce4b362854951
Štefan Hadbavný <stefan.hadbavny@student.tuke.sk>
2020-03-22 16:04:04 +0100

    update

--- ps2/hof.c
+++ ps2/hof.c
@@ -1,37 +1,51 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
 #include "hof.h"
+int cmp_by_score(const void* p1, const void* p2){
+    struct player* player1 = (struct player*)p1;
+    struct player* player2 = (struct player*)p2;
+
+    int result = player2->score-player1->score;
+    // printf("comparing %s vs %s = %d\n", person1->score, person2->score, result);
+    return result;
+}
 
 //bool save(const struct player list[], const int size){
 
 //}
 
+
+
 int load(struct player list[]){
 FILE *file =fopen(HOF_FILE,"r");
  if(file == NULL){
-        printf("Error open file.\n");
-        fclose(file);
+      
+        
         return -1;
 }
-
+ else{
 int i=0;
-int b=fscanf(file,"%s %d\n",list[i].name, &list[i].score);
- if (b==EOF)
-    {
-         fclose(file);
-        return -1;
-    }
+size_t size=0;
+
  
-for(i=1;i<10;i++){
-    b=fscanf(file,"%s %d\n",list[i].name, &list[i].score);
+for(i=0;i<10;i++){
+    int b=fscanf(file,"%s %d\n",list[i].name, &list[i].score);
     
-    
+    size++;
     if (b==EOF)
     {
         
         break;
     }
 }
+
+
+
+   qsort(list, size, sizeof(struct player), cmp_by_score);
+
     fclose(file);
-    return i;
+    return i;}
 }
