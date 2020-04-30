4d7ebb3b97a12d66f4b7584f1d1145817eb8540d
Štefan Hadbavný <stefan.hadbavny@student.tuke.sk>
2020-04-30 17:35:23 +0200

    update

--- ps4/bmp.c
+++ ps4/bmp.c
@@ -5,17 +5,15 @@
 #include <ctype.h>
 #include "bmp.h"
 int main(){
-    struct bmp_header* read_bmp_header(FILE *write_ptr);
+    struct bmp_header* read_bmp_header(FILE *stream);
 	
 }
-struct bmp_header* read_bmp_header(FILE *write_ptr){
-write_ptr = fopen("stream","wb");
-if(write_ptr == NULL){
-		return NULL;
-	}
+struct bmp_header* read_bmp_header(FILE *stream){
+
+
 	
 	struct bmp_header* header=malloc(sizeof(struct bmp_header));
-	if(fread (header, sizeof (struct bmp_header), 1, write_ptr) != 1 ) {
+	if(fread (header, sizeof (struct bmp_header), 1, stream) != 1 ) {
 		free(header);
 		return NULL;
 	}
@@ -23,11 +21,11 @@ if(write_ptr == NULL){
 		free(header);
 		return NULL;
 	}
-	printf("fdsfd");
-	printf("%d hhgh",header -> type);
-	fclose(write_ptr);
+	if(stream == NULL){
+		return NULL;
+	}
 	return header;
 }
-//struct pixel* read_data(FILE* write_ptr, const struct bmp_header* header){
+//struct pixel* read_data(FILE* stream, const struct bmp_header* header){
 
 //}
