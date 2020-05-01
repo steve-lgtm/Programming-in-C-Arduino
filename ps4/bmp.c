eb98aa7b0f7dd125e68c8119ffc1a6b7810b0bdd
Štefan Hadbavný <stefan.hadbavny@student.tuke.sk>
2020-05-01 16:43:09 +0200

    update

--- ps4/bmp.c
+++ ps4/bmp.c
@@ -24,6 +24,15 @@ struct bmp_header* read_bmp_header(FILE *stream){
 		free(header);
 		return NULL;
 	}
+	if(header -> width<=0) {
+		free(header);
+		return NULL;
+	}
+	if(header -> height<=0) {
+		free(header);
+		return NULL;
+	}
+
 	
 		
 	
@@ -32,3 +41,16 @@ struct bmp_header* read_bmp_header(FILE *stream){
 //struct pixel* read_data(FILE* stream, const struct bmp_header* header){
 
 //}
+
+void free_bmp_image(struct bmp_image* image){
+	if(image!=NULL){
+		
+		if(image->data!=NULL){
+			free(image->data);
+		}
+		if(image->header!=NULL){
+			free(image->header);
+		}
+		free(image);		
+	}
+}
